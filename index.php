ae86c861c0720620e34dfd63e88fb64b7c35dd1e
Pamella Candeia <98132313+PamellaCandeia@users.noreply.github.com>
2023-06-28 04:09:54 -0300

    Modificações PAM ^^

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require_once './views/header.php'
-?>
-<h1><a href="./views/newUser.php">Cadastrar Usuário</h1>
-<h1><a href="./views/login.php">Conectar</h1>
-
-<?php
-  require_once './views/footer.php'
-?>
