edc7bb4616ede6273b4eb937bf0c07c8ecd1ed6e
Raissa <37599780+ThiAmaral@users.noreply.github.com>
2023-07-07 05:00:29 -0300

    finalizar compra

--- PhotoFolio/views/exibirCarinho.php
+++ PhotoFolio/views/exibirCarinho.php
@@ -27,7 +27,8 @@ if (isset($_SESSION["carrinho"]) && sizeof($_SESSION["carrinho"]) != 0) { // pas
     </div><!-- End Page Header -->  
       <font face="Tahoma" color="white">
         <div align="center">
-        ---- BEBIDAS NO SISTEMA -----
+        ---- CARRINHO -----
+        <a href="../controllers/controllerCarrinho.php?opcao=4&total=<?= $soma ?>"><input type="button" value="Finalizar Compra"></a>
         
       
       <table border="1" align="center" cellspacing="2" cellpadding="1" width="80%"  class="table table-dark">
@@ -43,23 +44,23 @@ if (isset($_SESSION["carrinho"]) && sizeof($_SESSION["carrinho"]) != 0) { // pas
               </tr>
               
               <?php 
-              
-              
               foreach ($carrinho as $item){ ?>
                         
-                        <tr align="center">
-                            <td><?php echo $item->getbebida()->getBebida_id(); ?></td>
-                            <td><?php echo $item->getbebida()->getNome(); ?></td>
-                            <td><?php echo $item->getbebida()->getVolume(); ?></td>
-                            <td><?php echo $item->getbebida()->getPreco(); ?></td>
-                            <td><?php echo $item->getbebida()->getPeso(); ?></td>
-                            <td><?php echo $item->getbebida()->getEstoque(); ?></td>
-                            <td><?php echo $item->getbebida()->getFabricante(); ?></td>
-                            <td><a href='../controlers/controlerbebida.php?opcao=4&id=<?= $item->getbebida()->getBebida_id()?>'>Alterar</a>&nbsp;<a href='../controlers/controlerbebida.php?opcao=3&id=<?= $Bebida->getBebida_id()?>'>Excluir</a></td>
-                        </tr>
-                  
-
-              <?php } ?>
+                <tr align="center">
+                  <td><?php echo $item->getbebida()->getBebida_id(); ?></td>
+                  <td><?php echo $item->getbebida()->getNome(); ?></td>
+                  <td><?php echo $item->getbebida()->getVolume(); ?></td>
+                  <td><?php echo $item->getbebida()->getPreco(); ?></td>
+                  <td><?php echo $item->getbebida()->getPeso(); ?></td>
+                  <td><?php echo $item->getbebida()->getEstoque(); ?></td>
+                  <td><?php echo $item->getbebida()->getFabricante(); ?></td>
+                  <td><a href='../controlers/controlerbebida.php?opcao=4&id=<?= $item->getbebida()->getBebida_id()?>'>Alterar</a>&nbsp;<a href='../controlers/controlerbebida.php?opcao=3&id=<?= $Bebida->getBebida_id()?>'>Excluir</a></td>
+                </tr>
+              <?php
+                $soma += $item->getValorItem();
+              }
+               ?>
+               <a href="../controllers/controllerCarrinho.php?opcao=processoVenda&total=<?= $soma ?>"><input type="button" value="Finalizar Compra"></a>
               </div>
       </font>
       </table>
