d0c5864396ec2868dbe4b5a76d90972158cdb7af
Pamella Candeia <98132313+PamellaCandeia@users.noreply.github.com>
2023-07-07 22:06:32 -0300

    pam63

--- PhotoFolio/views/exibirCarinho.php
+++ PhotoFolio/views/exibirCarinho.php
@@ -1,14 +1,15 @@
 <?php
 
 require_once './includes/cabecalho.inc';
+require_once '../Classes/Item.inc.php';
 require_once '../classes/bebidasLoja.inc.php';
-session_start();
+require_once '../Classes/Cliente.inc.php';
+require_once '../Classes/cidade.inc.php';
+
+$frete = 0;
 $soma = 0;
-$i = 1;
-if (isset($_SESSION["carrinho"]) && sizeof($_SESSION["carrinho"]) != 0) { // passar a verificação para a controller
-     $carrinho = $_SESSION["carrinho"];
-     // Realizar o percurso no vetor de carrinho e colocar as informações em cada linha <tr>
-}
+$numerador = 1;
+
 
 ?>
 
@@ -28,42 +29,79 @@ if (isset($_SESSION["carrinho"]) && sizeof($_SESSION["carrinho"]) != 0) { // pas
       <font face="Tahoma" color="white">
         <div align="center">
         ---- CARRINHO -----
-        <a href="../controllers/controllerCarrinho.php?opcao=4&total=<?= $soma ?>"><input type="button" value="Finalizar Compra"></a>
-        
+
+      <?php
+
+            if(isset($_REQUEST['status'])){
+              echo "<p><font face='verdana' size='3' color='red' > Nenhum item foi incluido no carrinho de compras </font>";
+
+              echo "<p><a href='../controlers/controlerProduto.php?opcao=6'> Visualizar Produtos </a>";
+            }else{
+              session_start();
+              $carrinho = $_SESSION['carrinho'];
+              $cliente = $_SESSION['cliente'];
+              $cidades = $_SESSION['cidades'];
+            }
+
+      ?>
+
+ 
       
       <table border="1" align="center" cellspacing="2" cellpadding="1" width="80%"  class="table table-dark">
               <tr align="center">
+                  <th witdh="10%">N°</th>
                   <th witdh="10%">ID</th>
                   <th>Nome</th>
-                  <th>Volume</th>
-                  <th>Preco</th>
-                  <th>Kg</th>
-                  <th>Em Estoque</th>
                   <th>Fabricante</th>
-                  <th>Operação</th>
+                  <th>Quantidade</th>
+                  <th>Valor Item</th>
+                  <th>Em Estoque</th>
+                  <th>Valor Total</th>
+                  <th>Remover</th>
               </tr>
               
               <?php 
-              foreach ($carrinho as $item){ ?>
+              foreach ($carrinho as $item){
+                if($numerador%2) $color = '#cccccc';
+                else  $color = '#FFFFFF'  
+                ?>
                         
                 <tr align="center">
-                  <td><?php echo $item->getbebida()->getBebida_id(); ?></td>
-                  <td><?php echo $item->getbebida()->getNome(); ?></td>
-                  <td><?php echo $item->getbebida()->getVolume(); ?></td>
-                  <td><?php echo $item->getbebida()->getPreco(); ?></td>
-                  <td><?php echo $item->getbebida()->getPeso(); ?></td>
-                  <td><?php echo $item->getbebida()->getEstoque(); ?></td>
-                  <td><?php echo $item->getbebida()->getFabricante(); ?></td>
-                  <td><a href='../controlers/controlerbebida.php?opcao=4&id=<?= $item->getbebida()->getBebida_id()?>'>Alterar</a>&nbsp;<a href='../controlers/controlerbebida.php?opcao=3&id=<?= $item->getbebida()->getBebida_id()?>'>Excluir</a></td>
+                  <td><?= $numerador ?></td>
+                  <td><?= $item->getBebida()->getBebida_id(); ?></td>
+                  <td><?= $item->getBebida()->getNome(); ?></td>
+                  <td><?= $item->getBebida()->getFabricante(); ?></td>
+                  <td><?= $item->getQuantidade(); ?></td>
+                  <td>R$ <?= $item->getBebida()->getPreco(); ?></td>
+                  <td><?= $item->getBebida()->getEstoque(); ?></td>
+                  <td>R$ <?= $item->getValorItem(); ?></td>
+                  <td><button type="submit"><a href="../controlers/controlerCarrinho.php?opcao=2&index=<?= $numerador-1?>">Remover</a></button></td>
                 </tr>
               <?php
                 $soma += $item->getValorItem();
-              }
+
+                foreach($cidades as $cidadeCliente){
+                  if($cidadeCliente->getCidadeId() == $cliente->getCidade()){
+                    $frete = $cidadeCliente->getValorFrete();
+                  }
+                }
+
+                $somacomfrete = $soma + $frete;
+                $numerador++;
+              }//fim forech
+
                ?>
-               <a href="../controllers/controllerCarrinho.php?opcao=processoVenda&total=<?= $soma ?>"><input type="button" value="Finalizar Compra"></a>
+               <tr align="right"><td colspan="5"><font face="Verdana" size="4" color="red"><b>Frete = R$ <?= $cliente->getCidade()->getValorFrete(); ?></b></font></td></tr>
+               <tr align="right"><td colspan="5"><font face="Verdana" size="4" color="red"><b>Valor Total = R$ <?= $somacomfrete ?></b></font></td></tr>
+               
               </div>
       </font>
       </table>
+
+      <p>
+          
+          <a href="../views/ExibicaoParaVenda.php" class="btn-get-started">Continuar Comprando</a><br><br>
+          <a href="../controlers/controlerCarrinho.php?opcao=5&total=<?= $somacomfrete ?>" class="btn-get-started">Finalizar Compra</a>
 
    
  
